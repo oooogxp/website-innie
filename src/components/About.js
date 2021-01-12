75551ad3b698f2f68d5623afe9d46c943a4b2b01
Jeongin Yoon <oooogxp@gmail.com>
2021-01-13 01:05:44 +0900

    Combined all the css files to a single file

--- src/components/About.js
+++ src/components/About.js
@@ -8,9 +8,9 @@ class About extends Component {
                 <div className="pFont flex-align">
                     <p>개발자를 향한 첫 프로젝트 Website-innie 입니다.</p>
                     <p className="bold">프로젝트 목표</p>
-                    <ul>
-                        <li>프론트엔드 먼저 예쁘게 완성!</li>
-                        <li>strapi 와 연결하는 것 이해하기</li>
+                    <ul className="contents-border">
+                        <li> 프론트엔드 먼저 예쁘게 완성!</li>
+                        <li><a target="_blank" rel="noopener noreferrer" href="https://strapi.io/">strapi</a>와 연결하는 것 이해하기</li>
                         <li>한 달 안에 완성하기</li>
                     </ul>
                     <p>좋은 개발자가 되기 위한 2021년 첫 프로젝트 화이팅!</p>
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -24,14 +24,14 @@ class Blog extends Component {
                     <article className="flex-align border">
                         <h3>2021.01.12</h3>
                         <h1>Snowy Day!</h1>
-                        <p>지난주에 이어 오늘도 폭설이 내렸다. 순식간에 5cm는 쌓인 것 같다. 온 세상이 하얗다!<br/>
+                        <p className="line-height contents-width">지난주에 이어 오늘도 폭설이 내렸다. 순식간에 5cm는 쌓인 것 같다. 온 세상이 하얗다!<br/>
                         방송에서 제설 작업을 할 사람을 찾는다고 도움을 요청했다. 관리사무소 직원분들 일손이 모자른가보다.<br/>
                         내일 서울에 가야하는데 운전은 어려울 것 같다. 오늘 부디 아무 사고도 일어나지 않길!</p>
                     </article>
                     <article className="flex-align border">
                         <h3>2021.01.11</h3>
                         <h1>너무 졸리다</h1>
-                        <p>요즘(?) 아침에 눈 뜨는게 너무 힘들다.<br/>
+                        <p className="line-height contents-width">요즘(?) 아침에 눈 뜨는게 너무 힘들다.<br/>
                         1시 쯤이면 일찍 자는 편인 것 같은데, 내 몸은 하루 8시간 이상 꼬박 자 줘야 제 기능을 하는 듯 하다.<br/>
                         운동 부족도 원인이겠지. 그렇지만 운동은 너무 어려운 일! 잠이 조금만 줄었으면 좋겠다.<br/>
                         아니면 내 집중력이 2배로 높아져서 일과를 빨리 마치고 잘 수 있다면 내 죄책감이 조금은 덜 할 것 같다. 흑흑..</p>
@@ -39,7 +39,7 @@ class Blog extends Component {
                     <article className="flex-align border">
                         <h3>2021.01.10</h3>
                         <h1>애플 전기차</h1>
-                        <p>애플에서 전기 자동차를 만들 예정이라는 기사를 보고 너무 설렜다.<br/>
+                        <p className="line-height contents-width">애플에서 전기 자동차를 만들 예정이라는 기사를 보고 너무 설렜다.<br/>
                         테슬라와 애플카의 경쟁구도! 앞으로 더 좋은 전기차들을 많이 만나게 될 것 같아 기대된다.<br/>
                         애플 주식은 어디까지 오룰까! 한 주라도 제대로 가져보고 싶다.<br/></p>
                     </article>
