c8331b6315cee898c185ec95454b5fc64720ae95
Jeongin Yoon <oooogxp@gmail.com>
2021-02-16 22:10:53 +0900

    Added Threejs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.css';
-import Main from './components/Main';
-import Timeline from './components/Timeline';
-import Blog from './components/Blog';
-import About from './components/About';
-import Book from './components/Book';
-import { BrowserRouter, Route } from 'react-router-dom';
+// import Main from './components/Main';
+// import Timeline from './components/Timeline';
+// import Blog from './components/Blog';
+// import About from './components/About';
+// import Book from './components/Book';
+import Three from './components/Three';
+// import { BrowserRouter, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
-        <Main></Main>
+        {/* <Main></Main>
         <Route path="/about"><About></About></Route>
         <Route path="/timeline"><Timeline></Timeline></Route>
         <Route path="/blog"><Blog></Blog></Route>
-        <Route path="/book"><Book></Book></Route>
+        <Route path="/book"><Book></Book></Route> */}
+        <Three></Three>
       </div>
-    </BrowserRouter>
   );
 }
 
